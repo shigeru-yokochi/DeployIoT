a6494dd69052598e4f830836bdb1785bf33d5636
shigeru yokochi <shigeru@yokochi.jp>
2018-09-04 20:49:03 +0900

    test5

--- lambda_function.lambda_handler.py
+++ lambda_function.lambda_handler.py
@@ -7,7 +7,7 @@ iot = boto3.client('iot-data')
 def lambda_handler(event, context):
     topic = 'sdkTest/sub'
     payload = {
-        "message": "Lambda deploy test4"
+        "message": "Lambda deploy test5"
     }
     try:
         iot.publish(
