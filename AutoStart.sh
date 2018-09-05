084a09d4e3c1c69ce828d93f91cebcc5c0c6ddd6
shigeru yokochi <shigeru@yokochi.jp>
2018-09-05 12:56:06 +0900

    test5

---
+++ AutoStart.sh
@@ -0,0 +1,2 @@
+cd /root/DeployIoT
+./DeployIoT
--- DeployIoT.c
+++ DeployIoT.c
@@ -209,7 +209,7 @@ int main(int argc, char **argv) {
 		return rc;
 	}
 
-	IOT_INFO("Subscribing... test4");
+	IOT_INFO("Subscribing... test5");
 	rc = aws_iot_mqtt_subscribe(&client, "sdkTest/sub", 11, QOS0, iot_subscribe_callback_handler, NULL);
 	if(SUCCESS != rc) {
 		IOT_ERROR("Error subscribing : %d ", rc);
