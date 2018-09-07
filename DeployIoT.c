6efc0aa2dfbd7a70378a9dae66c9617b62601eee
shigeru yokochi <shigeru@yokochi.jp>
2018-09-07 13:51:22 +0900

    local_define.h作成

--- DeployIoT.c
+++ DeployIoT.c
@@ -1,30 +1,3 @@
-/*
- * Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
-/**
- * @file subscribe_publish_sample.c
- * @brief simple MQTT publish and subscribe on the same topic
- *
- * This example takes the parameters from the aws_iot_config.h file and establishes a connection to the AWS IoT MQTT Platform.
- * It subscribes and publishes to the same topic - "sdkTest/sub"
- *
- * If all the certs are correct, you should see the messages received by the application in a loop.
- *
- * The application takes in the certificate path, host name , port and the number of times the publish should happen.
- *
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -32,6 +5,7 @@
 #include <limits.h>
 #include <string.h>
 
+#include "aws_iot_config.h"
 #include "aws_iot_config.h"
 #include "aws_iot_log.h"
 #include "aws_iot_version.h"
